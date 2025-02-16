a7b29e81b59088193a6f4ea78382adc09b789615
Нина Чекалина <na.chekalina@interfax.ru>
2025-02-16 23:37:43 +0300

    change test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from cassandra.cluster import Cluster
 
 def test_insert_and_read():
     cluster = Cluster(['localhost'])
-    session = cluster.connect('my_keyspace')
+    session = cluster.connect('test_keyspace')
     
     session.execute("INSERT INTO users (id, name) VALUES (1, 'Katerina')")
     rows = session.execute("SELECT name FROM users WHERE id = 1")
