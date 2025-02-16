1f80a324f492d9e2f17d6999786e7c0186b253e1
Нина Чекалина <na.chekalina@interfax.ru>
2025-02-17 00:25:19 +0300

    change test.py

--- test.py
+++ test.py
@@ -5,10 +5,8 @@ from cassandra import ConsistencyLevel
 def test_insert_and_read():
     cluster = Cluster(['localhost'])
     session = cluster.connect('test_keyspace')
-	session.execute("INSERT INTO users (id, name) VALUES (1, 'Ekaterina')")
-
-
-
+	
+    session.execute("INSERT INTO users (id, name) VALUES (1, 'Ekaterina')")
     # Проверяем, что запись вставлена
     rows = session.execute("SELECT id, name FROM users")
 
