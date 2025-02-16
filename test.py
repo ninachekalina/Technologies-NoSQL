21efb964be09632988e5a78a5b54d75c3bf43104
Нина Чекалина <na.chekalina@interfax.ru>
2025-02-17 00:18:19 +0300

    изменен test.py

--- test.py
+++ test.py
@@ -5,13 +5,12 @@ from cassandra import ConsistencyLevel
 def test_insert_and_read():
     cluster = Cluster(['localhost'])
     session = cluster.connect('test_keyspace')
-	session.default_consistency_level = ConsistencyLevel.QUORUM
 	session.execute("INSERT INTO users (id, name) VALUES (1, 'Ekaterina')")
 
 
 
     # Проверяем, что запись вставлена
-    rows = session.execute("SELECT id, name FROM users WHERE id = 1")
+    rows = session.execute("SELECT id, name FROM users")
 
     for row in rows:
         print(f"Inserted row: id={row.id}, name={row.name}")  # Этот вывод появится в логах GitHub Actions
